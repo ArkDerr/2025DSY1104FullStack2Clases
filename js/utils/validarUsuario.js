89eff8d0487052a3a2cb09b9d6fd92173879cfcb
Servicios Digitales <Cetecom@SJAPCA03LC0801.duoc.acad>
2025-09-02 22:00:42 -0400

    S4_1

--- js/utils/validarUsuario.js
+++ js/utils/validarUsuario.js
@@ -6,18 +6,34 @@ let btnLogin = document.getElementById("btnLogin");
 const user = "Admin";
 const pass = "123456";
 
-btnLogin.addEventListener("click", () => {
+if (btnLogin && usuario && contrasena ){
+    btnLogin.addEventListener("click", (e) => {
+        e.preventDefault();
     if (usuario.value == user && contrasena.value == pass ) {
-        
+        localStorage.setItem("logueado","true");
+        localStorage.setItem("usuario",usuario.value);
         window.location.href = "index.html";
-        let txtLogin = document.getElementById("txtLogin");
-        let txtRegistrarse = document.getElementById("txtRegistrarse");
-        txtLogin.innerText = "";
-        txtRegistrarse.innerText = "";
-
     } else {
         //alert("Usuario o contraseña incorrecta")
         const modal = new bootstrap.Modal(document.getElementById("loginError"));
         modal.show();
+    }
+});
+}
+
+document.addEventListener("click", (e) =>{
+    const clicLogout = e.target.closest("#btnLogout, #nav-logout a");
+    if(!clicLogout) return;
+    e.preventDefault();
+    localStorage.removeItem("logueado");
+    localStorage.removeItem("usuario");
+
+    const header = document.getElementById("header");
+    header?.querySelector("nav-login")?.classList.remove("d-none");
+    header?.querySelector("nav-registro")?.classList.remove("d-none");
+    header?.querySelector("nav-logout")?.classList.add("d-none");
+
+    if(!location.pathname.endsWith("index.html")){
+        window.location.href="index.html";
     }
 });
